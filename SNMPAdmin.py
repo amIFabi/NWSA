d5c99e2699e200ecaaba35e163f36843690e61f7
Fabi <fabperaltar@gmail.com>
2019-09-19 11:25:57 -0500

    SNMP admin functions

--- SNMPAdmin.py
+++ SNMPAdmin.py
@@ -30,26 +30,24 @@ def dumpRRD(rrdFile, xmlFile):
 	print("RRD dumped")
 
 def snmpGet(community, host, oid):
-	errorIndication, errorStatus, errorIndex, varBinds = getCmd(SnmpEngine(), 
-																															CommunityData(community), 
-																															UdpTransportTarget((host, 161)), 
-																															ContextData(), 
-																															ObjectType(ObjectIdentity(oid)))
+	varBinds = getCmd(SnmpEngine(), 
+						CommunityData(community), 
+						UdpTransportTarget((host, 161)), 
+						ContextData(), 
+						ObjectType(ObjectIdentity(oid)))
 
-	if errorIndication:
-		print(errorIndication)
-	elif errorStatus:
-		print('%s at %s' % (errorStatus.prettyPrint(), errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-	else:
-		for varBind in varBinds:
-			varB = (' = '.join([x.prettyPrint() for x in varBind]))
-			resultado = varB.split()[2]
+	mib = str(next(varBinds)[3][0])
 	
-	return resultado
-	#return next(varBinds)
+	return mib.split(sep='=')[1]
 
-def updateRRD(host, version, community, port):
+def updateRRD(host, community, oids, port):
+	row = "N:"
 	while 1:
+		
+		for i in range(len(oids)):
+			row = row + snmpGet(community, host, oids[i])
+
+		"""
 		# ifOutUcastPkts. Tabla. Revisar
 		paquetes_unicast_interfaz = int(snmpGet(community, host, '1.3.6.1.2.1.2.2.1.17.'+port))
 
@@ -67,7 +65,9 @@ def updateRRD(host, version, community, port):
 
 		valor = "N:" + str(paquetes_unicast_interfaz) + ':' + str(paquetes_input_ipv4) + ':' + str(mensajes_icmmp_echo) + ':' + str(total_input_traffic) + ':' + str(datagrama_udp)
     #print(valor)
-		rrdtool.update(host + ".rrd", valor)
+		"""
+
+		rrdtool.update(host + ".rrd", row)
 		rrdtool.dump(host + ".rrd", host + ".xml")
 		time.sleep(1)
 
@@ -136,7 +136,7 @@ def createPDF(host):
 			break
 
     #agente[0] - host
-    #agente[1] - version
+    #agente[1] 
     #agente[2] - comunidad
     #agente[3] - puerto
 
@@ -180,10 +180,21 @@ addDataSource("192.168.1.79.rrd", "DS:inTcp:COUNTER:600:U:U")
 addDataSource("192.168.1.79.rrd", "DS:datagramUDP:COUNTER:600:U:U")
 dumpRRD("192.168.1.79.rrd", "192.168.1.79.xml")
 #print(snmpGet("EricFabianPeraltaRamirez", "10.31.87.154", "1.3.6.1.2.1.1.1.0")[3][0].)
-
-g = getCmd(SnmpEngine(), CommunityData("fabo"), UdpTransportTarget(("127.0.0.1", 161)), ContextData(), ObjectType(ObjectIdentity("1.3.6.1.2.1.1.1.0")))
-
-print(next(g)[3][0])
 """
+
+#g = getCmd(SnmpEngine(), CommunityData("fabo"), UdpTransportTarget(("127.0.0.1", 161)), ContextData(), ObjectType(ObjectIdentity("1.3.6.1.2.1.1.1.0")))
+
+#print(next(g)[3][0])
+
+createRRD("localhost")
+
+print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.1.1.0"))
+print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.2.2.1.17.8"))
+print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.4.3.0"))
+print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.5.8.0"))
+print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.6.10.0"))
+print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.7.1.0"))
+
+
 
 
