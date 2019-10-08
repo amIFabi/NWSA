92ca86ae9b88199e3d29de226b18b3f3bfaaedb3
Fabi <fabperaltar@gmail.com>
2019-10-08 16:58:53 -0500

    SNMP admin

--- SNMPAdmin.py
+++ SNMPAdmin.py
@@ -1,23 +1,27 @@
-import sys, rrdtool, time
+import sys, rrdtool, time, os, MongoAdmin
 from pysnmp.hlapi import *
 from reportlab.pdfgen import canvas
 
-def createRRD(rrdName):
-	rrdFile = rrdName + ".rrd"
-	xmlFile = rrdName + ".xml"
-	ret = rrdtool.create(rrdFile, "--start", "N", "--step", "60",
-											"DS:inUnicastInterfaz:COUNTER:600:U:U",
-											"DS:inIpv4:COUNTER:600:U:U",
-											"DS:inEchoSsnmp:COUNTER:600:U:U",
-											"DS:inTcp:COUNTER:600:U:U",
-											"DS:datagramUDP:COUNTER:600:U:U",
-											"RRA:AVERAGE:0.5:1:600")
+def createRRD(rrdName, step="60", ds=["DS:ifOutUcastPkts:COUNTER:600:U:U"], rra="RRA:AVERAGE:0.5:1:600"):
+	rrdFile = "rrd/" + rrdName + ".rrd"
+	xmlFile = "xml/" + rrdName + ".xml"
+	ret = rrdtool.create(rrdFile, "--start", "N", "--step", step, ds[0], rra)
+	if(len(ds) > 1):
+		for i in range(1, len(ds)):
+			addRRA(rrdFile, ds[i])
+	
+	#"DS:ipInReceives:COUNTER:600:U:U",
+	#"DS:icmpInEchoes:COUNTER:600:U:U",
+	#"DS:tcpInSegs:COUNTER:600:U:U",
+	#"DS:udpInDatagrams:COUNTER:600:U:U",
 
 	dumpRRD(rrdFile, xmlFile)
 	if ret:
 		print(rrdtool.error())
 	else:
+		print("--------------------------------------------------")
 		print("RRD created")
+		print("--------------------------------------------------")
 
 def addDataSource(rrdFile, dsSpec):
 	rrdtool.tune(rrdFile, dsSpec)
@@ -27,7 +31,9 @@ def addRRA(rrdFile, rraSpec):
 
 def dumpRRD(rrdFile, xmlFile):
 	rrdtool.dump(rrdFile, xmlFile)
+	print("--------------------------------------------------")
 	print("RRD dumped")
+	print("--------------------------------------------------")
 
 def snmpGet(community, host, oid):
 	varBinds = getCmd(SnmpEngine(), 
@@ -38,163 +44,128 @@ def snmpGet(community, host, oid):
 
 	mib = str(next(varBinds)[3][0])
 	
-	return mib.split(sep='=')[1]
+	return mib.split(sep=' = ')[1]
 
-def updateRRD(host, community, oids, port):
-	row = "N:"
+def updateRRD(agent):
 	while 1:
+		x = MongoAdmin.getHost(agent)
+		if(x == None):
+			break
+		value = []
+		row = "N:"
+		oids = MongoAdmin.getOIDS()
+		for mib in oids:
+			if(mib["if"]):
+				resp = snmpGet(x["community"], x["hostaddr"], (mib["oid"] + "." + x["if"]))
+			else:
+				resp = snmpGet(x["community"], x["hostaddr"], mib["oid"])
+			
+			if(resp == "No Such Object currently exists at this OID"):
+				resp = "0"
+
+			value.append(resp)
+
+		row = row + ":".join(value)
+		#print(row)
 		
-		for i in range(len(oids)):
-			row = row + snmpGet(community, host, oids[i])
-
-		"""
-		# ifOutUcastPkts. Tabla. Revisar
-		paquetes_unicast_interfaz = int(snmpGet(community, host, '1.3.6.1.2.1.2.2.1.17.'+port))
-
-    # ipInReceives. Escalar
-		paquetes_input_ipv4 = int(snmpGet(community, host, '1.3.6.1.2.1.4.3.0'))
-
-		# icmpInEchoes. Escalar
-		mensajes_icmmp_echo = int(snmpGet(community, host, '1.3.6.1.2.1.5.8.0'))
-
-		# tcpInSegs. Escalar
-		total_input_traffic = int(snmpGet(community, host, '1.3.6.1.2.1.6.10.0'))
-
-		# udpInDatagrams. Escalar
-		datagrama_udp = 0
-
-		valor = "N:" + str(paquetes_unicast_interfaz) + ':' + str(paquetes_input_ipv4) + ':' + str(mensajes_icmmp_echo) + ':' + str(total_input_traffic) + ':' + str(datagrama_udp)
-    #print(valor)
-		"""
-
-		rrdtool.update(host + ".rrd", row)
-		rrdtool.dump(host + ".rrd", host + ".xml")
+		rrdtool.update("rrd/" + x["rrdName"] + ".rrd", row)
+		rrdtool.dump("rrd/" + x["rrdName"] + ".rrd", "xml/" + x["rrdName"] + ".xml")
 		time.sleep(1)
 
-def graphRRD(minutes, host):
+def graphRRD(minutes, rrdName):
 	aux = int(minutes)
 	tiempoA = int(time.time())
 	tiempoInicio = tiempoA - (60 * aux)  # 5 min = 600 s
 
-	ret1 = rrdtool.graph(host+"_1.png", "--start", str(tiempoInicio),
+	oids = MongoAdmin.getOIDS()
+	for x in oids:
+		rrdtool.graph("graphs/" + rrdName + x["name"] + ".png", "--start", str(tiempoInicio),
+									"--vertical-label=Bytes/s",
+									"--width", "500",
+									"--height", "250",
+									"--zoom", "10",
+									"--full-size-mode",
+									"--title=" + x["desc"],
+									"DEF:input=rrd/" + rrdName + ".rrd:" + x["name"] + ":AVERAGE",
+									"AREA:input#00FF00:" + x["name"] + " \r")
+
+	"""
+	ret1 = rrdtool.graph(rrdName+"_1.png", "--start", str(tiempoInicio),
 											"--vertical-label=Bytes/s",
 											"--width", "500",
 											"--height", "250",
 											"--zoom", "10",
 											"--full-size-mode",
 											"--title=Paquetes Unicast que ha recibido la interfaz",
-											"DEF:entrada="+host+".rrd:inUnicastInterfaz:AVERAGE",
+											"DEF:entrada="+rrdName+".rrd:inUnicastInterfaz:AVERAGE",
 											"AREA:entrada#00FF00:Paquetes Unicast \r")
 
-	ret2 = rrdtool.graph(host + "_2.png", "--start", str(tiempoInicio),
+	ret2 = rrdtool.graph(rrdName + "_2.png", "--start", str(tiempoInicio),
 												"--vertical-label=Bytes/s",
 												"--width", "500",
 												"--height", "250",
 												"--zoom", "10",
 												"--full-size-mode",
 												"--title=Paquetes recibidos a protocolos PIv4",
-												"DEF:entrada=" + host + ".rrd:inIpv4:AVERAGE",
+												"DEF:entrada=" + rrdName + ".rrd:inIpv4:AVERAGE",
 												"AREA:entrada#00FF00:Paquetes IPv4 \r")
 
-	ret3 = rrdtool.graph(host + "_3.png", "--start", str(tiempoInicio),
+	ret3 = rrdtool.graph(rrdName + "_3.png", "--start", str(tiempoInicio),
 												"--vertical-label=Bytes/s",
 												"--width", "500",
 												"--height", "250",
 												"--zoom", "10",
 												"--full-size-mode",
 												"--title=Mensajes ICMP echo que ha enviado el agente",                      
-												"DEF:entrada=" + host + ".rrd:inEchoSsnmp:AVERAGE",
+												"DEF:entrada=" + rrdName + ".rrd:inEchoSsnmp:AVERAGE",
 												"AREA:entrada#00FF00:Mensajes ICMP \r")
 
-	ret4 = rrdtool.graph(host + "_4.png", "--start", str(tiempoInicio),
+	ret4 = rrdtool.graph(rrdName + "_4.png", "--start", str(tiempoInicio),
 											"--vertical-label=Bytes/s",
 											"--width", "500",
 											"--height", "250",
 											"--zoom", "10",
 											"--full-size-mode",
 											"--title=Segmentos TCP recibidos",
-											"DEF:entrada=" + host + ".rrd:inTcp:AVERAGE",
+											"DEF:entrada=" + rrdName + ".rrd:inTcp:AVERAGE",
 											"AREA:entrada#00FF00:Segmentos TCP \r")
 
-	ret5 = rrdtool.graph(host + "_5.png", "--start", str(tiempoInicio),
+	ret5 = rrdtool.graph(rrdName + "_5.png", "--start", str(tiempoInicio),
 											"--vertical-label=Bytes/s",
 											"--width", "500",
 											"--height", "250",
 											"--zoom", "10",
 											"--full-size-mode",
 											"--title=Datagramas entragados a usuarios UDP",
-											"DEF:entrada=" + host + ".rrd:datagramUDP:AVERAGE",
+											"DEF:entrada=" + rrdName + ".rrd:datagramUDP:AVERAGE",
 											"AREA:entrada#00FF00:Datagramas UDP \r")
+	"""
 
-def createPDF(host):
-	archivo = open("agentes.txt", "r")  # Archivo de texto - escritura al final
-	texto = archivo.read()  # Tenemos el archivo completo
-	lista = texto.split("\n")
-	for aux in lista:
-		agente = aux.split(" ")
-		if agente[0] == host:
-			break
+def createPDF(agent):
+	oids = MongoAdmin.getOIDS()
 
-    #agente[0] - host
-    #agente[1] 
-    #agente[2] - comunidad
-    #agente[3] - puerto
+	ops = snmpGet(agent["community"], agent["hostaddr"], "1.3.6.1.2.1.1.1.0")
+	loc = snmpGet(agent["community"], agent["hostaddr"], "1.3.6.1.2.1.1.6.0")
+	time = int(snmpGet(agent["community"], agent["hostaddr"], "1.3.6.1.2.1.1.3.0"))
+	numports = snmpGet(agent["community"], agent["hostaddr"], "1.3.6.1.2.1.2.1.0")
 
-	# sysDescr
-	sistema_operativo = str(snmpGet(agente[2].replace(" ", ""), host.replace(" ", ""), '1.3.6.1.2.1.1.1.0'))
+	time = int(time // 60)
 
-	# sysLocation
-	ubicacion = str(snmpGet(agente[2].replace(" ", ""), host.replace(" ", ""), '1.3.6.1.2.1.1.6.0'))
-
-	tiempo_actividad = int(snmpGet(agente[2].replace(" ", ""), host.replace(" ", ""), '1.3.6.1.2.1.1.3.0'))
-
-	num_puertos = str(snmpGet(agente[2].replace(" ", ""), host.replace(" ", ""), '1.3.6.1.2.1.2.1.0'))
-
-	pdf = canvas.Canvas("reporte-"+host+".pdf")
+	pdf = canvas.Canvas(agent["rrdName"] + ".pdf")
 	pdf.setLineWidth(.3)
 	pdf.setFont('Helvetica', 12)
 
-	pdf.drawString(30, 750, "OS: " + sistema_operativo)
-	pdf.drawString(30, 735, "Location: " + ubicacion)
-	pdf.drawString(30, 720, "Interfaces: " + num_puertos)
-	pdf.drawString(30, 705, "Up time: " +str(tiempo_actividad)+ " segundos" )
-	pdf.drawString(30, 690, "Community: " + agente[2])
-	pdf.drawString(30, 675, "IP: " + agente[0])
+	pdf.drawString(30, 750, "OS: " + ops)
+	pdf.drawString(30, 735, "Location: " + loc)
+	pdf.drawString(30, 720, "Interfaces: " + numports)
+	pdf.drawString(30, 705, "Up time: " + str(time) + " min." )
+	pdf.drawString(30, 690, "Community: " + agent["community"])
+	pdf.drawString(30, 675, "IP: " + agent["hostaddr"])
 
-	pdf.drawImage(agente[0] + "_1.png", 30, 555, 250, 100)
-	pdf.drawImage(agente[0] + "_2.png", 290, 555, 250, 100)
-	pdf.drawImage(agente[0] + "_3.png", 30, 450, 250, 100)
-	pdf.drawImage(agente[0] + "_4.png", 290, 450, 250, 100)
-	pdf.drawImage(agente[0] + "_5.png", 30, 345, 250, 100)
+	for i in oids:
+		pdf.drawImage("graphs/" + agent["rrdName"] + agent["name"] + ".png", 30, 555, 250, 100)
 
 	pdf.save()
 
-	estatus_agente = str(snmpGet("fabo", "127.0.0.1", '1.3.6.1.2.1.2.2.1.8.1'))
-	print("Agent Status: " + str(estatus_agente))
-
-"""
-createRRD("192.168.1.79")
-addDataSource("192.168.1.79.rrd", "DS:inIpv4:COUNTER:600:U:U")
-addDataSource("192.168.1.79.rrd", "DS:inEchoSsnmp:COUNTER:600:U:U")
-addDataSource("192.168.1.79.rrd", "DS:inTcp:COUNTER:600:U:U")
-addDataSource("192.168.1.79.rrd", "DS:datagramUDP:COUNTER:600:U:U")
-dumpRRD("192.168.1.79.rrd", "192.168.1.79.xml")
-#print(snmpGet("EricFabianPeraltaRamirez", "10.31.87.154", "1.3.6.1.2.1.1.1.0")[3][0].)
-"""
-
-#g = getCmd(SnmpEngine(), CommunityData("fabo"), UdpTransportTarget(("127.0.0.1", 161)), ContextData(), ObjectType(ObjectIdentity("1.3.6.1.2.1.1.1.0")))
-
-#print(next(g)[3][0])
-
-createRRD("localhost")
-
-print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.1.1.0"))
-print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.2.2.1.17.8"))
-print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.4.3.0"))
-print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.5.8.0"))
-print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.6.10.0"))
-print(snmpGet("fabo", "localhost", "1.3.6.1.2.1.7.1.0"))
-
-
-
-
+	#agentstatus = str(snmpGet("fabo", "127.0.0.1", '1.3.6.1.2.1.2.2.1.8.1'))
+	#print("Agent Status: " + str(agentstatus))
